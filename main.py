5248c9bf97656835867c0904f86adce8e8f555f6
Mikey Autorino <autorinomikey@gmail.com>
2026-06-28 20:37:12 -0500

    basic working done

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen3-0.6B")
 model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen3-0.6B")
 try:
     while True:
-        prompt = input("Type Something Here: \n")
+        prompt = input("Type Something Here:\n")
         messages = [
             {
             "role" : "system",
@@ -17,8 +17,10 @@ try:
         ]
         text = tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True, enable_thinking=False)
         model_inputs = tokenizer([text], return_tensors="pt").to(model.device)
-        generated_ids = model.generate(**model_inputs, max_new_tokens=50)
-        print(tokenizer.batch_decode(generated_ids[:, input_length:], skip_special_tokens=True)[0])
+        generated_ids = model.generate(**model_inputs, max_new_tokens=500)
+        output_ids = generated_ids[0][len(model_inputs.input_ids[0]):].tolist() 
+        content = tokenizer.decode(output_ids, skip_special_tokens=True).strip("\n")
+        print(content)
 except KeyboardInterrupt:
     print("\n exited")
 
