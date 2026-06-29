400d91e30d61b121a2c1c44bef346e23cc22259b
Mikey Autorino <autorinomikey@gmail.com>
2026-06-28 20:10:49 -0500

    input formatting from documentation

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
-from transformers import pipeline
+from transformers import AutoTokenizer, AutoModelForCausalLM
 import sys
 
-
+tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen3-0.6B")
+model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen3-0.6B")
 try:
     while True:
+        prompt = input("Type Something Here: \n")
+        messages = [
+            {
+            "role" : "system",
+            "content": "You are a chatbot that always answers like a wize wizard",
+            },
+            {
+            "role" : "user",
+            "content" : prompt,
+            }
+        ]
+        
         pass
 except KeyboardInterrupt:
     print("\n exited")
